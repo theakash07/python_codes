ad0d6c1aa60b365cffc6731c235c41cfdae052a6
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-06-21 16:10:57 +0530

    area of circle

---
+++ circlearea.py
@@ -0,0 +1,4 @@
+import math #importin math module and math.pi will automatically fetch the value of math.pi =3.14
+radius= int(input("Enter radius:"))
+area= math.pi*radius**2
+print("The area is ",area)
