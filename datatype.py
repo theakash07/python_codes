d7061cf8c1916625e7286fb8828e02654977ff04
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-06-21 18:43:24 +0530

    data types of python

---
+++ datatype.py
@@ -0,0 +1,19 @@
+var_1=12
+var_2=10.1
+print(var_1+var_2)
+print(type(var_1))
+print(type(var_2))
+var_3=0x24 #can print hexaedecimal number also and conversion on real time
+print(var_3)
+print(type(var_3))
+var_4=0b10110100101010#conversion of binary number also on real time
+print(var_4)
+print(type(var_4))
+#there are several data types in python 
+#int
+#float
+#string
+#boolean
+#list
+#tuple
+
