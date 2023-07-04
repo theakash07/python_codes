2b0e55a65eb38ca58a172ca3766f0329cd13786e
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-04 05:36:06 +0530

    Update sortestcalculator.py

--- sortestcalculator.py
+++ sortestcalculator.py
@@ -1,1 +1,8 @@
 Expression=int(),str(),int()
+
+#can use this too 
+expression=input("Expression: ")
+print("{:.1f}".format(eval(expression),1))
+#eval function is used to evaluate the expression in a string
+#formal function is used for formatting
+
