65f96a90085e3bd5c0e3061ab459929fae805b85
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-04 08:03:02 +0530

    Delete circlearea.py

--- circlearea.py
+++
@@ -1,4 +0,0 @@
-import math #importin math module and math.pi will automatically fetch the value of math.pi =3.14
-radius= int(input("Enter radius:"))
-area= math.pi*radius**2
-print("The area is ",area)
