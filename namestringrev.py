7fb6fd4078a8e866e3a29c4cebd17cd37a05d8f8
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-04 08:44:36 +0530

    reverse of name string

---
+++ namestringrev.py
@@ -0,0 +1,14 @@
+#program to reverse the name like akash kumar to kumar akash
+#Approach numebr-1 
+Name=input("Enter Name")
+first,last=Name.split(" ")#splitting the name by whitesace..
+print(first)
+print(last)
+print(last,first)
+
+
+#Approach number-2
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello  " + lname + " " + fname)
+
