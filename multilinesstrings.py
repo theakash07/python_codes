e5389448fef916c91a93183d2a8d3c5cdf1fc5f4
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-04 08:02:25 +0530

    multi line strings using python

---
+++ multilinesstrings.py
@@ -0,0 +1,7 @@
+#multi line strings using 3 double colon
+print("""Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are""")
