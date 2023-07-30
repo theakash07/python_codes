4fb858d2f9471609920f64910aebdc1d7e411b82
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-29 22:48:09 -0700

    random integer guessing

---
+++ random.py
@@ -0,0 +1,13 @@
+import random
+
+target_num = random.randint(1, 10)
+guess_num = 0
+print(target_num)
+while target_num != guess_num:
+    try:
+        guess_num = int(input('Guess a number between 1 and 10 until you get it right: '))
+    except ValueError:
+        print('Invalid input! Please enter a valid number.')
+        continue
+
+print('Well guessed!')
